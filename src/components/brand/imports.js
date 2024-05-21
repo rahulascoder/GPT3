7b4e6b305ac6efbc5e8e84d0af316dd077d15711
rahulascoder <rahulascoder85@gmail.com>
2024-05-21 13:39:52 +0530

    first commit

---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import google from '../../Asset/google.png';
+import slack from '../../Asset/slack.png';
+import atlassian from '../../Asset/atlassian.png';
+import dropbox from '../../Asset/dropbox.png';
+import shopify from '../../Asset/shopify.png';
+
+export{
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify
+};
---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import blog01 from '../../Asset/blog01.png';
+import blog02 from '../../Asset/blog02.png';
+import blog03 from '../../Asset/blog03.png';
+import blog04 from '../../Asset/blog04.png';
+import blog05 from '../../Asset/blog05.png';
+
+export {
+  blog01,
+  blog02,
+  blog03,
+  blog04,
+  blog05,
+};
